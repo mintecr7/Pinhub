1b08563e444445a69d396cf55837c9e839f55054
minte <mintesnotshiferaw@gmail.com>
2022-10-25 17:53:53 +0900

    initial push

---
+++ social/schemas/gasolin.js
@@ -0,0 +1,17 @@
+export default  {
+    name: 'gasoline',
+    title: 'Gasoline',
+    type: 'document',
+    fields: [
+        {
+            name: 'darkTimes',
+            title: 'Darktimes',
+            type: 'image'
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'comment'
+        },
+    ]
+}
